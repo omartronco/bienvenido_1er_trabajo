9fd77aebeefac0a278657b90162f787bd2eee5a6
Omar Said López Tronco <omartronco@iMac-de-Omar.local>
2017-12-22 13:26:44 -0600

    Initial commit

---
+++ app/src/main/java/mx/com/clupp/bienvenido_1er_trabajo/MainActivity.java
@@ -0,0 +1,13 @@
+package mx.com.clupp.bienvenido_1er_trabajo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
